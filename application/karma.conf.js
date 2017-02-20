a8d1180b52ea56c8ae0e8a166ff8729c8819ae17
Sunil  Jhamnani <sjhamnani@skytrace.local>
2017-02-21 01:00:58 +0530

    CRNS-75: completed Unit testing setup using Jasmine and Karma

---
+++ application/karma.conf.js
@@ -0,0 +1,80 @@
+// Karma configuration
+// Generated on Fri Feb 17 2017 13:37:09 GMT+0530 (IST)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path that will be used to resolve all patterns (eg. files, exclude)
+    basePath: './www',
+
+
+    // frameworks to use
+    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+    frameworks: ['jasmine'],
+
+
+    // list of files / patterns to load in the browser
+    files: [
+        'lib/ionic/js/angular/angular.js',
+        'lib/ionic/js/angular/angular-*.js',
+        'lib/ionic/js/angular/sinon-1.15.0.js',
+        'lib/ionic/js/angular-ui/angular-ui-router.js',
+        'lib/ionic/js/ionic.bundle.min.js',
+        'test/unit/**/*.js',
+        'js/*.js',
+        '**/*.js'
+    ],
+
+
+    // list of files to exclude
+    exclude: [
+    ],
+
+
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+    },
+
+
+    // test results reporter to use
+    // possible values: 'dots', 'progress'
+    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+    reporters: ['progress'],
+
+
+    // web server port
+    port: 9876,
+
+
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
+
+
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_DEBUG,
+
+
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
+
+
+    // start these browsers
+    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    browsers: ['Chrome'],
+
+    plugins: [
+      'karma-chrome-launcher',
+      'karma-jasmine'
+    ],
+
+    // Continuous Integration mode
+    // if true, Karma captures browsers, runs the tests and exits
+    singleRun: true,
+
+    // Concurrency level
+    // how many browser should be started simultaneous
+    concurrency: Infinity
+  })
+}
