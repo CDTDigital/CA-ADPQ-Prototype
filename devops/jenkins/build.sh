59f2a81b3fa33ac5539aa4266f7ed382464dad1e
Umesh Mehta <umesh.mehta@intimetec.com>
2017-03-03 17:33:34 +0530

    Parameterized build.sh, deploy.sh and added README.MD

--- devops/jenkins/build.sh
+++ devops/jenkins/build.sh
@@ -9,7 +9,7 @@ mvn clean install
 popd
 mv "${WORKSPACE}/webservice/website/target/ROOT.war" ROOT.war
 docker build -f ${WORKSPACE}/devops/docker/dockerfile .
-docker build --rm --pull=true -t deepakintime/cts:latest .
+docker build --rm --pull=true -t $userid_dockerhub/$registry_dockerhub:latest .
 docker login -u $userid_dockerhub -p password_dockerhub  -e $email_id_dockerhub
 docker push $userid_dockerhub/$registry_dockerhub:latest
 docker logout
